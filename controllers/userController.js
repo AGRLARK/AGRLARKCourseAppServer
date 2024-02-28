c2a7b877f24d904973fbc8d82fcde768618d13cb
anuraggupta <anuraggupta8309@gmail.com>
2024-02-28 21:42:37 +0530

    Created Register API

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,29 @@
-import Course from "./../model/User.js";
+import { catchAsyncError } from "./../middleware/catchAsyncError.js";
+import ErrorHandler from "./../utils/ErrorHandler.js";
+import User from "./../model/User.js";
+import { SendToken } from "./../utils/SendToken.js";
 
-export const getAllCourses = async (req, res, next) => {
-  const courses = await Course.find();
-  res.status(200).json({
-    success: true,
-    courses,
+export const register = catchAsyncError(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  // const file = req.file;
+
+  if (!name || !email || !password)
+    return next(new ErrorHandler("Please fill all the fields", 400));
+
+  let user = await User.findOne({ email });
+
+  if (user) return next(new ErrorHandler("User Already Exist ", 409));
+
+  //Upload file on cloudinary
+  user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "tempid",
+      url: "tempurl",
+    },
   });
-};
+  SendToken(res, user, "Registered Successfully", 201);
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,21 @@
 import express from "express";
+import { register } from "./../controllers/userController.js";
 
 const router = express.Router();
+
+router.route("/register").post(register);
+
+// Login
+// Logout
+// Get my Profile
+// Change Password
+// Update Profile
+// Update Profile Picture
+
+// Forget Password
+// Reset Password
+
+// AddtoPlaylist
+// RemoveFromPlaylist
 
 export default router;
