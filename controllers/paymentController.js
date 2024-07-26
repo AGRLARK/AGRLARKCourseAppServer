5a42cb5cd048569590e1dfc8c3761eca34682488
anuraggupta <anuraggupta8309@gmail.com>
2024-07-26 09:11:21 +0530

    Payment Verification API Done

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,6 +1,9 @@
 import { catchAsyncError } from "../middleware/catchAsyncError.js";
 import User from "../model/User.js";
+import { instance } from "../server.js";
 import ErrorHandler from "../utils/ErrorHandler.js";
+import crypto from "crypto";
+import { Payment } from "./../model/Payment.js";
 
 export const buySubscription = catchAsyncError(async (req, res, next) => {
   const user = await User.findById(req.user._id);
@@ -8,12 +11,12 @@ export const buySubscription = catchAsyncError(async (req, res, next) => {
   if (user.role === "admin")
     return next(new ErrorHandler("Admin can't buy subscription", 400));
 
-  const plan_id = process.env.PLAN_ID || "plan_7wAosPWtrkhqZw";
+  const plan_id = process.env.PLAN_ID || "plan_OWm8sldcxDDzLF";
 
   const subscription = await instance.subscriptions.create({
     plan_id,
     customer_notify: 1,
-    total_count: 6,
+    total_count: 12,
   });
 
   user.subscription.id = subscription.id;
@@ -24,6 +27,47 @@ export const buySubscription = catchAsyncError(async (req, res, next) => {
 
   res.status(201).json({
     success: true,
-    subscription,
+    subscriptionId: subscription.id,
+  });
+});
+
+export const paymentVerification = catchAsyncError(async (req, res, next) => {
+  const { razorpay_signature, razorpay_payment_id, razorpay_order_id } =
+    req.body;
+
+  const user = await User.findById(req.user._id);
+
+  const subscription_id = user.subscription.id;
+
+  const generated_signature = crypto
+    .createHmac("sha256", process.env.RAZOR_API_KEY)
+    .update(razorpay_order_id + "|" + subscription_id, "utf-8")
+    .digest("hex");
+
+  const isAuthentic = generated_signature === razorpay_signature;
+
+  if (!isAuthentic)
+    return res.redirect(`${process.env.FRONTEND_URL}/paymentfailed`);
+
+  // Database comes here
+  await Payment.create({
+    razorpay_signature,
+    razorpay_payment_id,
+    razorpay_order_id,
+  });
+
+  user.subscription.status = "active";
+
+  await user.save();
+
+  res.redirect(
+    `${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`
+  );
+});
+
+export const getRazorPayKey = catchAsyncError(async (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    key: process.env.RAZOR_API_KEY,
   });
 });
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,10 +1,20 @@
 import express from "express";
 import { isAuthenticated } from "../middleware/auth.js";
-import { buySubscription } from "./../controllers/paymentController.js";
+import {
+  buySubscription,
+  getRazorPayKey,
+  paymentVerification,
+} from "./../controllers/paymentController.js";
 
 const router = express.Router();
 
 // Buy Subscription
 router.route("/subscribe").get(isAuthenticated, buySubscription);
+
+// Verify payment and save reference in database
+router.route("/paymentverification").post(isAuthenticated, paymentVerification);
+
+// Get Razorpay Key
+router.route("/razorpaykey").get(getRazorPayKey);
 
 export default router;
